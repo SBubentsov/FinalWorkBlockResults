3591c5da8cba63347b7f634eca9ba970ce99c5e7
bubentsov-s <s.bubentsov@mail.ru>
2022-12-07 20:17:42 +0300

    работа с FinalWork - добавление метода создания строчного одномерного массива со случайными значениями ячеек

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -6,3 +6,45 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+
+string[] CreateArrayRndString(int size)
+{
+    Random rnd = new Random();
+
+    string[] array = new string[]
+    {
+            "hello", "BOOOO!", "Yes", "No", "bye", "=')", "Russia", "USA", "O_O", "Japan", "Homework", "Git", "Lol", "fun", "Dotnet", "X_X", "Canada", "UK", "FBR", "NOOB!", "?"
+    };
+    Console.Write("[");
+    for (int i = 0; i < size; i++)
+    {
+        int index = rnd.Next(size);
+        string entry = array[index];
+        if (i < size - 1) Console.Write($"{entry}| ");
+        else Console.Write($"{entry}] =>");
+    }
+    
+    return array;
+}
+
+
+void PrintArray(string[] arrayString)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        Console.Write($"\"{arrayString[i]}\"");
+        if (i < arrayString.Length - 1) Console.Write("| ");
+    }
+    Console.Write("]");
+}
+
+Console.Clear();
+
+Console.WriteLine("Введите количество элементов массива:");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+
+string[] arrayStrings = CreateArrayRndString(arraySize);
+// PrintArray(arrayStrings);
